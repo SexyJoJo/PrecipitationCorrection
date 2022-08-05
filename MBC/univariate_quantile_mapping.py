a3c1fd8fd2cacdf5e18de342b1ddce8f7e45246a
qiaochenwei <qiaochenwei@nuist.edu>
2022-08-05 15:38:58 +0800

    单变量订正

--- MBC/univariate_quantile_mapping.py
+++ MBC/univariate_quantile_mapping.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy.ma as ma
 import numpy as np
 from scipy import stats, interpolate
 from ncParseUtils import *
@@ -97,10 +98,10 @@ class QDM:
 
 
 def mse(y1, y2):
-    return (np.abs(y1 - y2)).mean()
+    return (np.square(y1 - y2)).mean()
 
 
-def main():
+def single_train():
     """
 
     :return:
@@ -108,8 +109,10 @@ def main():
     obs, remove = ObsParser.get_many_pravg(r"../divide area/divided obs", 2013, 2018, "JSJ", 4)
     case = CaseParser.get_many_pravg(r"../divide area/divided case", 2013, 2018, "JSJ", 0, remove)
 
-    test_obs, _ = ObsParser.get_one_pravg(r"../divide area/divided obs/JSJ_obs_prec_rcm_201904.nc")
-    test_case = CaseParser.get_one_pravg(r"../divide area/divided case/JSJ_PRAVG_2019030200c01_2019_monthly.nc", 0, remove)
+    test_obs_filename = ObsParser.get_filename("2019", "04", "JSJ")
+    test_case_filename = CaseParser.get_filename("2019", "0302", "JSJ", "01")
+    test_obs, _ = ObsParser.get_one_pravg(os.path.join(r"../divide area/divided obs/", test_obs_filename))
+    test_case = CaseParser.get_one_pravg(os.path.join(r"../divide area/divided case", test_case_filename), 0, remove)
 
     uqdm = QDM(case, obs)
     model = uqdm.predict(test_case)  # 得到订正后的y
@@ -120,5 +123,62 @@ def main():
     print("np.corrcoef(test_obs, model):", np.corrcoef(test_obs, model))
 
 
+def loop_train():
+    corr_cases, test_obses = [], []
+    for syear in range(1991, 2015):
+        # 训练集
+        #   真实值
+        obs, remove = ObsParser.get_many_pravg(r"../divide area/divided obs", syear, syear+4, "JSJ", 4)
+        case = CaseParser.get_many_pravg(r"../divide area/divided case", syear, syear+4, "JSJ", 0, remove)
+
+        # 测试集
+        test_obs_filename = ObsParser.get_filename(str(syear+5), "04", "JSJ")
+        test_case_filename = CaseParser.get_filename(str(syear+5), "0302", "JSJ", "01")
+        test_obs, _ = ObsParser.get_one_pravg(os.path.join(r"../divide area/divided obs", test_obs_filename))
+        test_case = CaseParser.get_one_pravg(os.path.join(r"../divide area/divided case", test_case_filename), 0, remove)
+
+        # 训练模型
+        uqdm = QDM(case, obs)
+
+        # 订正
+        corr_case = uqdm.predict(test_case)  # 得到订正后的case
+        plt.plot(test_case, color='blue')
+        plt.plot(corr_case, color='orange')
+        plt.plot(test_obs, color="black")
+        plt.show()
+        plt.close()
+
+        corr_cases.append(corr_case)
+        test_obses.append(test_obs)
+
+        print("---------------------")
+        print(f"year range:{syear}-{syear+4}")
+        print("mse(obs, case):", mse(obs, case))
+        print("mse(test_obs, model):", mse(test_obs, corr_case))
+        print("np.corrcoef(obs, case):", np.corrcoef(obs, case))
+        print("np.corrcoef(test_obs, model):", np.corrcoef(test_obs, corr_case))
+
+    all_obs, remove = ObsParser.get_many_pravg(r"../divide area/divided obs", 1991, 2018, "JSJ", 4)
+    all_cases = CaseParser.get_many_pravg(r"../divide area/divided case", 1991, 2018, "JSJ", 0, remove)
+    tcc = OtherUtils.cal_TCC(corr_cases, all_cases, test_obses, all_obs)
+
+    # ACC相关
+    part_obses, remove = ObsParser.get_many_pravg(r"../divide area/divided obs", 1996, 2019, "JSJ", 4)
+    part_cases = CaseParser.get_many_pravg(r"../divide area/divided case", 1996, 2019, "JSJ", 0, remove)
+    part_obses = OtherUtils.d_2_2d(part_obses, len(corr_cases[0]))  # 1996 - 2019
+    part_cases = OtherUtils.d_2_2d(part_cases, len(corr_cases[0]))  # 1996 - 2019
+
+    case_acc = OtherUtils.cal_ACC(part_cases, part_obses)
+    corr_case_acc = OtherUtils.cal_ACC(corr_cases, test_obses)
+    acc_img = PaintUtils.paint_ACC(range(1996, 2020), case_acc, corr_case_acc)
+    acc_img.show()
+
+    print("---------------------")
+    print("TCC:", tcc)
+    print("ACC:", corr_case_acc)
+    print("CASE ACC:", case_acc)
+
+
 if __name__ == '__main__':
-    main()
+    # single_train()
+    loop_train()
