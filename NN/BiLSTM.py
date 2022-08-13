3e709145566d58da4119c2746580a39c9e6bab70
jojo <996103492@qq.com>
2022-08-13 21:59:25 +0800

    算法学习

--- NN/BiLSTM.py
+++ NN/BiLSTM.py
@@ -11,10 +11,15 @@ sentence = (
     'from continuous integration and delivery to issue triage and more'
 )
 
+# 算法过程（分类问题：所有的词中选一个作为预测值）：
+# 'Github ? ? ? ... ?' => 'Actions'
+# 'Github Actions ? ? ? ... ?' => 'makes'
+# ...
+
 word2idx = {w: i for i, w in enumerate(list(set(sentence.split())))}
 idx2word = {i: w for i, w in enumerate(list(set(sentence.split())))}
-n_class = len(word2idx)  # classification problem
-max_len = len(sentence.split())
+n_class = len(word2idx)  # 分类的个数
+max_len = len(sentence.split())    # 21
 n_hidden = 5
 
 
@@ -24,8 +29,8 @@ def make_data(sentence):
 
     words = sentence.split()
     for i in range(max_len - 1):
-        input = [word2idx[n] for n in words[:(i + 1)]]
-        input = input + [0] * (max_len - len(input))
+        input = [word2idx[n] for n in words[:(i + 1)]]  # 把输入单词列表转为单词索引列表
+        input = input + [0] * (max_len - len(input))    # 其余单词补为0
         target = word2idx[words[i + 1]]
         input_batch.append(np.eye(n_class)[input])
         target_batch.append(target)
@@ -33,7 +38,7 @@ def make_data(sentence):
     return torch.Tensor(input_batch), torch.LongTensor(target_batch)
 
 
-# input_batch: [max_len - 1, max_len, n_class]
+# input_batch维度: [max_len-1(循环的次数), max_len, n_class(每个单词的编码长度)]
 input_batch, target_batch = make_data(sentence)
 dataset = Data.TensorDataset(input_batch, target_batch)
 loader = Data.DataLoader(dataset, 16, True)
@@ -42,7 +47,7 @@ class BiLSTM(nn.Module):
     def __init__(self):
         super(BiLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size=n_class, hidden_size=n_hidden, bidirectional=True)
-        # fc
+        # 全连接
         self.fc = nn.Linear(n_hidden * 2, n_class)
 
     def forward(self, X):
@@ -50,10 +55,12 @@ class BiLSTM(nn.Module):
         batch_size = X.shape[0]
         input = X.transpose(0, 1)  # input : [max_len, batch_size, n_class]
 
-        hidden_state = torch.randn(1*2, batch_size, n_hidden)   # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
-        cell_state = torch.randn(1*2, batch_size, n_hidden)     # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        hidden_state = torch.randn(1*2, batch_size, n_hidden)   # 维度[num_layers(BiLSTM层数) * num_directions(双向，取2), batch_size, n_hidden]
+        cell_state = torch.randn(1*2, batch_size, n_hidden)     # 维度[num_layers(BiLSTM层数) * num_directions(双向，取2), batch_size, n_hidden]
 
-        outputs, (_, _) = self.lstm(input, (hidden_state, cell_state))
+        # 全部输出
+        outputs, (_, _) = self.lstm(input, (hidden_state, cell_state))  # 维度[max_len, batch_size, n_hidden * 2]
+        # 最后一个细胞的输出
         outputs = outputs[-1]  # [batch_size, n_hidden * 2]
         model = self.fc(outputs)  # model : [batch_size, n_class]
         return model
