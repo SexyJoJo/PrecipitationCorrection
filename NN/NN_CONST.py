aaf0f767e27438c64d900ab9e9989c5c983aae17
jojo <996103492@qq.com>
2023-05-19 10:22:47 +0800

    去除两种模型都保存的策略， 新增早停策略

--- NN/NN_CONST.py
+++ NN/NN_CONST.py
@@ -20,11 +20,12 @@ TRAIN_END_YEAR = 2018
 # 数据处理参数
 DATA_ENHANCE = 0  # 数据增强以增加样本量，仅针对二维
 NORMALIZATION = 'zscore'  # [minmax, zscore]
-DATA_FORMAT = 'grid11'  # [map, grid11, gird33]
-MODEL = 'ANN'  # [LSTM_CNN, ANN, ANN_h16, ANN33, LSTM, LSTM11, UNet]
+DATA_FORMAT = 'map'  # [map, grid11, gird33]
+MODEL = 'LSTM11'  # [LSTM_CNN, ANN, ANN_h16, ANN33, LSTM, LSTM11, UNet]
 
 # 训练参数
-EPOCH = 150
+EARLY_STOP = False
+EPOCH = 400
 if DATA_FORMAT == 'map' and DATA_ENHANCE:
     BATCH_SIZE = 32
 elif DATA_FORMAT == 'map':
@@ -38,8 +39,8 @@ LR = 0.001
 
 # 输出根目录
 # DESCRIPTION = "LSTM2_CNN1-原始值-zscore-lr.005-最小损失模型epoch150-输入n月输出n月"
-# DESCRIPTION = "ANN-原始值-zscore-最小损失模型-输入n月3乘3输出1月1乘1-.0005lr-去dropout"
-DESCRIPTION = f"{MODEL}-原始值-{NORMALIZATION}-最小损失模型-验证测试分开-输入{DATA_FORMAT}" \
+# DESCRIPTION = "LSTM-原始值-zscore-最小损失模型-输入n月1乘1输出1月1乘1-batch1024-epoch150-.001lr"
+DESCRIPTION = f"{MODEL}-{'早停模型' if EARLY_STOP else '最终模型'}-验证测试分开-输入{DATA_FORMAT}" \
               f"{'增强'+str(DATA_ENHANCE*8+1)+'倍' if DATA_ENHANCE else ''}-" \
               f"batch{BATCH_SIZE}-epoch{EPOCH}-{LR}lr"
 LOSS_PATH = rf"./output/{DESCRIPTION}/loss"
--- NN/train.py
+++ NN/train.py
@@ -11,21 +11,57 @@ from torch.utils.data import DataLoader
 from NN_CONST import *
 from visdom import Visdom
 
-torch.manual_seed(42)
+
+class EarlyStopping:
+    def __init__(self, patience=7, verbose=False, delta=0):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        # 查找损失最小模型
+        os.makedirs(MODEL_PATH + rf"/{DATE}/{CASE_NUM}/{TIME}/{BASIN}", exist_ok=True)
+        best_model_path = MODEL_PATH + rf"/{DATE}/{CASE_NUM}/{TIME}/{BASIN}/{AREA}_{TRAIN_START_YEAR}-" \
+                                       rf"{TRAIN_END_YEAR}年模型(除{test_year}年).pth"
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model, best_model_path)  # save checkpoint
+        self.val_loss_min = val_loss
 
 
 def train():
     viz = Visdom(env='PR')
-    # 窗口初始化
-    # viz.line([0.], [0], win='train_loss', opts=dict(title='train_loss'))
-    # viz.line([[0., 0.]], [0],
-    #          win='tt_loss', update='append',
-    #          opts=dict(title='train_test_loss', legend=['trainloss', 'testloss']))
+    early_stopping = EarlyStopping(patience=7, verbose=True)
 
-    total_training_loss_list = []  # 用于绘制损失趋势图
-    total_test_loss_list = []
-
-    best_model_loss = float('inf')
     for epoch in range(EPOCH):
         # 模型训练
         model.train()
@@ -40,8 +76,8 @@ def train():
             loss.backward()
             optimizer.step()
             training_loss += loss.item()
-            print(f"epoch:{epoch}  i:{i}   loss:{loss.item()}  "
-                  f"total_training_loss:{training_loss}")
+            # print(f"epoch:{epoch}  i:{i}   loss:{loss.item()}  "
+            #       f"total_training_loss:{training_loss}")
         training_one_loss = training_loss / len(train_dataloader)
         loss_epoch.append(training_one_loss)
 
@@ -53,50 +89,42 @@ def train():
                 valid_inputs, valid_labels = data
                 # test_inputs, test_labels = test_inputs.to(device), test_labels.to(device)
                 valid_outputs = model(valid_inputs)
-                test_loss = criterion(valid_outputs, valid_labels)
-                testing_loss += test_loss
+                valid_loss = criterion(valid_outputs, valid_labels)
+                testing_loss += valid_loss
 
         testing_loss = testing_loss.item() / len(test_dataloader)
         loss_epoch.append(testing_loss)
 
-        # 查找损失最小模型
-        os.makedirs(MODEL_PATH + rf"/{DATE}/{CASE_NUM}/{TIME}/{BASIN}", exist_ok=True)
-        best_model_path = MODEL_PATH + rf"/{DATE}/{CASE_NUM}/{TIME}/{BASIN}/{AREA}_{TRAIN_START_YEAR}-" \
-                                       rf"{TRAIN_END_YEAR}年模型(除{test_year}年)_best.pth"
-        if testing_loss < best_model_loss and epoch > 20:
-            best_model_loss = testing_loss
-            torch.save(model, best_model_path, _use_new_zipfile_serialization=False)
-
+        # visdom中绘制损失图
         viz.line([[training_one_loss, testing_loss]], [epoch],
                  win=f'tt_loss_{test_year}', update='append',
                  opts=dict(title=f'train_test_loss_{test_year}', legend=['trainloss', 'testloss']))
-        # print(f"epoch:{epoch}   testing_loss:{test_loss.item()}  "
+
+        # 早停
+        if EARLY_STOP:
+            early_stopping(valid_loss, model)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+        # print(f"epoch:{epoch}   testing_loss:{valid_loss.item()}  "
         #       f"total_testing_loss:{testing_loss}")
         # for name, param in model.state_dict().items():
         #     print(f"{name}: {param}")
 
-        total_training_loss_list.append(training_loss)
-        total_test_loss_list.append(testing_loss)
+        # total_training_loss_list.append(training_loss)
+        # total_test_loss_list.append(testing_loss)
 
     final_model_path = MODEL_PATH + rf"/{DATE}/{CASE_NUM}/{TIME}/{BASIN}/{AREA}_{TRAIN_START_YEAR}-" \
                                     rf"{TRAIN_END_YEAR}年模型(除{test_year}年).pth"
     torch.save(model, final_model_path, _use_new_zipfile_serialization=False)
 
-    # 绘制损失图
-    plt.plot(list(range(EPOCH)), total_training_loss_list, label="total training loss")
-    plt.plot(list(range(EPOCH)), total_test_loss_list, label="total test loss")
-    # plt.ylim([0, 0.002])
-    plt.legend()
-    os.makedirs(LOSS_PATH, exist_ok=True)
-    plt.savefig(LOSS_PATH + rf"/{AREA}_{TRAIN_START_YEAR}-{TRAIN_END_YEAR}年损失(除{test_year}).png")
-    plt.close()
-    # return model
-
 
 if __name__ == '__main__':
+    # 设定种子
+    torch.manual_seed(42)
     # 日志初始化
     logging.basicConfig(filename='./log.txt', level=logging.DEBUG, format='%(asctime)s  %(message)s')
-    # setup_seed(20)
     # 所有年份中选一年作为测试集，一年作为验证集，其他年份作为训练集，以不同的训练集循环训练多个模型
     for test_year in range(TRAIN_START_YEAR + 1, TRAIN_END_YEAR + 1):
         valid_year = test_year - 1
